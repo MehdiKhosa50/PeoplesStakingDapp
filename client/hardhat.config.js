692f6714029f8526eb39a1e8718ce92c8aef0537
Muhammad Muntazir Mehdi <mehdikhosa50@gmail.com>
2024-09-16 02:20:35 +0500

    Added Hardhat Network

--- client/hardhat.config.js
+++ client/hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-};
+  
+  networks: {
+    // Local Hardhat network configuration
+    hardhat: {
+      chainId: 1337, // Set the chain ID for consistency
+    },
+  }
+}
